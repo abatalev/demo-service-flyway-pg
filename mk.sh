57580dc9b718ed0f50fdee88b3ad6ff2941c0e83
andrey <andrey.batalev@gmail.com>
2025-03-25 21:16:01 +0300

    dbservice: added Dockerfile

--- mk.sh
+++ mk.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+VERSION="0.0.1"
+CDIR=$(pwd)
+
 echo "Demo DbService with Flyway"
-cd build/dbservice || exit
+
+echo "### buiild jar for dbservice"
+cd "${CDIR}/build/dbservice" || exit
 mvn clean install
+
+echo "### create docker image for dbservice"
+cd "${CDIR}/build" || exit
+if ! docker buildx build -f Dockerfile.dbservice -t "abatalev/dbservice:${VERSION}" .; then
+    echo "### aborted"
+fi
+
+docker run --rm -p 8080:8080 abatalev/dbservice:0.0.1
