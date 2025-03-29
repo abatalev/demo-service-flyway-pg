b77cd81fbd0f8d9eea28eafcd0f8bded419e8364
andrey <andrey.batalev@gmail.com>
2025-03-29 11:39:30 +0300

    dbservice: separate PostgresAdepter

--- build/dbservice/src/test/java/com/abatalev/demo/dbservice/DbTest.java
+++ build/dbservice/src/test/java/com/abatalev/demo/dbservice/DbTest.java
@@ -2,52 +2,28 @@ package com.abatalev.demo.dbservice;
 
 import static org.junit.Assert.assertEquals;
 
-import org.flywaydb.core.Flyway;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
+
+import com.abatalev.demo.dbservice.utils.PostgresAdapter;
 
 @Testcontainers
 public class DbTest {
 
-    @Container
-    private static PostgreSQLContainer container = new PostgreSQLContainer<>(
-        DockerImageName
-            .parse("abatalev/postgres:0.0.1")
-            .asCompatibleSubstituteFor("postgres"))
-            .withPassword("postgres");
-
-    static DriverManagerDataSource dataSource;       
+    @ClassRule
+    private static PostgresAdapter adapter;
     
     @BeforeAll
     static void migrate() {
-
-        String url =  "jdbc:postgresql://"+container.getHost()+":"+container.getMappedPort(5432)+"/test_db?loggerLevel=OFF";
-
-        var flyway = Flyway.configure()
-            .locations("filesystem:./target/db/migrations")
-            .schemas("test_schema")
-            .dataSource(url, "test_admin", "qwerty")
-            .load();
-        flyway.info();
-        flyway.migrate();
-
-        dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(url);
-        dataSource.setUsername("test_admin");
-        dataSource.setPassword("qwerty");
-        dataSource.setSchema("test_schema");
+        adapter = new PostgresAdapter();
     }
 
     @Test 
     void checkQuery(){
-        int result = new JdbcTemplate(dataSource).queryForObject("SELECT COUNT(*) FROM a", Integer.class);
+        int result = new JdbcTemplate(adapter.getDataSource()).queryForObject("SELECT COUNT(*) FROM a", Integer.class);
         assertEquals(0L,result);
     }        
 }
---
+++ build/dbservice/src/test/java/com/abatalev/demo/dbservice/utils/PostgresAdapter.java
@@ -0,0 +1,50 @@
+package com.abatalev.demo.dbservice.utils;
+
+import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class PostgresAdapter {
+
+    static private Logger log = LoggerFactory.getLogger(PostgresAdapter.class);
+    private DriverManagerDataSource dataSource; 
+    
+    public PostgresAdapter() {
+        PostgreSQLContainer container = new PostgreSQLContainer<>(
+            DockerImageName
+                .parse("abatalev/postgres:0.0.1")
+                .asCompatibleSubstituteFor("postgres"))
+                .withPassword("postgres");
+        container.start();
+        
+        String url =  "jdbc:postgresql://"+container.getHost()+":"+container.getMappedPort(5432)+"/test_db?loggerLevel=OFF";
+        log.info(url);
+
+        System.setProperty("DB_HOST", container.getHost());
+        System.setProperty("DB_PORT", ""+container.getMappedPort(5432));
+        System.setProperty("DB_USER", "test_user");
+        System.setProperty("DB_PASS", "qwerty");
+
+        var flyway = Flyway.configure()
+            .locations("filesystem:./target/db/migrations")
+            .schemas("test_schema")
+            .dataSource(url, "test_admin", "qwerty")
+            .load();
+        flyway.info();
+        flyway.migrate();
+
+        dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername("test_admin");
+        dataSource.setPassword("qwerty");
+        dataSource.setSchema("test_schema"); 
+    } 
+
+    public DriverManagerDataSource getDataSource() {
+        return dataSource;
+    }
+}
