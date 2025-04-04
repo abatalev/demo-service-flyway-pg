ffa258cec6b76b69251cffcbb16767a73f45c5d8
andrey <andrey.batalev@gmail.com>
2025-04-04 14:10:42 +0300

    k6: modify test

--- build/generator/test.js
+++ build/generator/test.js
@@ -1,24 +1,32 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-import { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
-import { Trend } from 'k6/metrics';
-
-const things_waiting_time = new Trend('things_waiting_time');
 
 export const options = {
-  duration: '4h',
-  thresholds: {
-    checks: ['rate == 1.00'],
-  },
-};
+    scenarios: {
+      getAll: {
+        executor: 'shared-iterations',
+        exec: 'all',
+        vus: 80,
+        iterations: 1000,
+      },
+      newThing: {
+        executor: 'shared-iterations',
+        exec: 'newThing',
+        vus: 20,
+        iterations: 1000,
+      },
+    },
+  };
 
-export default function () {
-  describe('Get things list!', () => {
-    const response = http.get('http://dbservice:8080/things');
-    things_waiting_time.add(response.timings.waiting);
-    expect(response.status, 'response status').to.equal(200);
-    expect(response).to.have.validJsonBody();
-    // expect(response.json('ratings'), 'ratings list').to.be.an('array');
+  export function all() {
+    http.get('http://dbservice:8080/things');
     sleep(1)
-  });
-}
+  }
+  
+  export function newThing() {
+    let res = http.post('http://dbservice:8080/things/ivanov', 
+        JSON.stringify({ name: 'iPhone' }), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+    sleep(3)
+  }
